b3fbcb84800e53ea75cc130a1db9a07b425206b8
Kyaw Thu <ktkt2009@users.noreply.github.com>
2015-02-16 10:44:53 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,19 +2,19 @@
 ## It is concerned with lexical scoping and caching functions
 # makeCacheMatrix
 makeCacheMatrix <- function(x = matrix()) {
-  matt <- NULL
+  matX <- NULL
   set <- function(y) {
     x <<- y
-    matt <<- NULL
+    matX <<- NULL
   }
   get <- function() {           # get return matrix
     x
   }
   setSolve <- function(solve) { # Function to get the inversed matrix from a special object created by makeCacheMatrix.
-    matt <<- solve
+    matX <<- solve
   }
   getSolve <- function() {      # return cache inverse matrix 
-    matt
+    matX
   }
   list(set = set, get = get, setSolve = setSolve, getSolve = getSolve)  #Returned object (actually it's a list) contains methods:
 }
@@ -22,15 +22,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
-## If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
+##  retrieve the inverse from the cache.
 cacheSolve <- function(x, ...) {
-  matt <- x$getSolve()
-  if(!is.null(matt)) {
+  matX <- x$getSolve()
+  if(!is.null(matX)) {
     message("getting cached data")
-    return(matt)
+    return(matX)
   }
   data <- x$get()
-  matt <- solve(data, ...)
-  x$setSolve(matt)
-  matt
+  matX <- solve(data, ...)
+  x$setSolve(matX)
+  matX              #  invesres matrix from the cache
 }
